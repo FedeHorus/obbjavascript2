9d2f70fb090930bd9ac59672c6ba3449788dd6ea
FedeHorus <110682482+FedeHorus@users.noreply.github.com>
2022-09-19 11:26:12 -0300

    Add files via upload

---
+++ ejercicio2.js
@@ -0,0 +1,18 @@
+
+//Javascript ejercicio2
+let nombre = 'Federico';
+let edad= 28;
+let desarrollador = true;
+let fnacimiento = new Date(1994, 07, 26); //
+
+const libro = {
+    titulo : "",
+    autor: "",
+    fecha: new Date(),
+    url: "",
+    
+
+
+}
+
+console.log(fnacimiento)
